e103eb3bcf9e0c3fcb8e0b3d87d3b1128db27985
valigton <valigtonverissimo@zsystems.com.br>
2020-03-26 02:39:10 -0300

    Front-end da aplicação finalizado

---
+++ frontend/src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Logon from './pages/Logon/index'
+import Register from './pages/Register/index'
+import Profile from './pages/Profile/index'
+import NewIncident from './pages/NewIncident/index'
+
+export default function Routes() {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/incidents/new" exact component={NewIncident} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
